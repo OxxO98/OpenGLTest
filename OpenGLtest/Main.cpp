45179813c1e9a41551db4207ae630d58bed7669e
OxxO98 <hbok1119@naver.com>
2020-06-01 20:13:39 +0900

    일단 필요 없는거 삭제

--- OpenGLtest/Main.cpp
+++ OpenGLtest/Main.cpp
@@ -13,7 +13,6 @@ void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode);
 
 void processInput(GLFWwindow* window);
-unsigned int* defineVertexArray();
 
 void windowSizeCallback(GLFWwindow* window, int width, int height);
 
@@ -173,101 +172,4 @@ void setTextoNDC(int len, float* NDC, float* Tex) {
 		NDC[i * 8 + 6] = Tex[i * 2];
 		NDC[i * 8 + 7] = Tex[i * 2 + 1];
 	}
-}
-
-unsigned int* defineVertexArray() {
-	
-	int scBox[] = {
-		720, 720, 0,
-		720, 0, 0,
-		0, 0, 0,
-		0, 720, 0
-	};
-
-	float scBoxc[] = {
-		1.0f, 0.0f, 0.0f,
-		0.0f, 1.0f, 0.0f,
-		1.0f, 0.0f, 1.0f,
-		1.0f, 1.0f, 0.0f
-	};
-
-	float scBoxt[] = {
-		1.0f, 1.0f,
-		1.0f, 0.0f,
-		0.0f, 0.0f,
-		0.0f, 1.0f
-	};
-
-	float vertices[32];
-
-	SCtoNDC(12, scBox, vertices);
-	//showVector(3, vertices);
-	setColortoNDC(12, vertices, scBoxc);
-	//showVector(6, vertices);
-	setTextoNDC(24, vertices, scBoxt);
-	//showVector(8, vertices);
-
-	unsigned int indices[] = {  // 0부터 시작한다는 것을 명심하세요!
-		0, 1, 3,   // 첫 번째 삼각형
-		1, 2, 3    // 두 번째 삼각형
-	};
-
-	unsigned int EBO;
-	unsigned int VBO, VAO;
-	glGenVertexArrays(1, &VAO);
-	glGenBuffers(1, &VBO);
-	glGenBuffers(1, &EBO);
-
-	glBindVertexArray(VAO);
-
-	glBindBuffer(GL_ARRAY_BUFFER, VBO);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-
-	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
-
-	//위치
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0);
-	glEnableVertexAttribArray(0);
-	//컬러
-	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(3 * sizeof(float)));
-	glEnableVertexAttribArray(1);
-	//texture attrubute
-	glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(6 * sizeof(float)));
-	glEnableVertexAttribArray(2);
-
-
-	//texture
-	unsigned int texture;
-	glGenTextures(1, &texture);
-	glBindTexture(GL_TEXTURE_2D, texture);
-	//set texture wrapping
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
-	//set Texture
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-	//image load
-	int width, height, channel;
-	stbi_set_flip_vertically_on_load(true);
-	unsigned char* data = stbi_load("img/asdf2-h.png", &width, &height, &channel, STBI_rgb_alpha);
-	if (data) {
-		std::cout << "일단 데이터는 가져옴\n" << std::endl;
-		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
-		glGenerateMipmap(GL_TEXTURE_2D);
-	}
-	else {
-		std::cout << "데이터 못가져옴" << std::endl;
-	}
-	stbi_image_free(data);
-
-
-	//texture bind
-	glBindTexture(GL_TEXTURE_2D, texture);
-
-	//render shader
-	glBindVertexArray(VAO);
-
-	unsigned int ret[] = { VAO, EBO };
-	return ret;
 }
