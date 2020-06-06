e2b7ee96de41cd4a0a8062b31c2c3a39eff4f724
OxxO98 <hbok1119@naver.com>
2020-06-06 11:20:00 +0900

    전투화면 추가
    일단 알수 없음 오류가 나긴하는데
    큰 신경은 안써도 될것같음

--- OpenGLtest/RenderStorage.cpp
+++ OpenGLtest/RenderStorage.cpp
@@ -8,7 +8,8 @@ void RenderStorage::LoadRenderStorage(std::vector<RenderObj>* pageData, std::str
 		pageData->push_back(RenderObj(glm::vec2(968.0f, 528.0f), glm::vec2(225, 105), "event", "event"));
 	}
 	if (page == "BattlePage") {
-		pageData->push_back(RenderObj(glm::vec2(780.0f, 457.0f), glm::vec2(225, 105), "set", "set"));
-		pageData->push_back(RenderObj(glm::vec2(968.0f, 528.0f), glm::vec2(225, 105), "event", "event"));
+		pageData->push_back(RenderObj(glm::vec2(0.0f, 0.0f), glm::vec2(1280, 720), "battleBackground", "battleBackground"));
+		pageData->push_back(RenderObj(glm::vec2(146.0f, 382.0f), glm::vec2(110, 202), "character", "character"));
+		pageData->push_back(RenderObj(glm::vec2(459.0f, 264.0f), glm::vec2(110, 202), "character", "character"));
 	}
 }
