2007f6d38d9a2162f52045f6d44be03cb7cd622e
OxxO98 <hbok1119@naver.com>
2020-06-11 14:49:39 +0900

    일단 전투원 목록 구성

--- OpenGLtest/cTest.c
+++ OpenGLtest/cTest.c
@@ -1,10 +1,29 @@
 #include <string.h>
+#include <stdio.h>
+#include "cTest.h"
 
-float cTest(const char* key) {
-	if (key == "up") {
-		return -10.0f;
+UserCharacter userCharacters[20];
+
+void initUserCharacters() {
+	for (int i = 0; i < 20; i++) {
+		userCharacters[i].CID = 1;
+		userCharacters[i].level = 20;
+		userCharacters[i].exp = 30;
+		userCharacters[i].attkPower = 100;
+		userCharacters[i].dfnsPower = 150;
 	}
-	else if (key == "down") {
-		return 10.0f;
+}
+
+void showUserCharacters() {
+	for (int i = 0; i < 20; i++) {
+		showUserCharacter(userCharacters[i]);
 	}
+}
+
+void showUserCharacter(UserCharacter character) {
+	printf("%d	", character.CID);
+	printf("%d	", character.level);
+	printf("%d	", character.exp);
+	printf("%d	", character.attkPower);
+	printf("%d\n", character.dfnsPower);
 }
